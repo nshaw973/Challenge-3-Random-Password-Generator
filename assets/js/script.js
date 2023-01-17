99c27704841a83c957f4643b3c84f3b4b56965f8
nshaw973 <nshaw973@gmail.com>
2023-01-17 15:29:45 -0800

    Added variables for the characters being used for the generator, and variables needed for the prompts length and options chosen.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+//added the getPrompt function
 function writePassword() {
+  getPrompts();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +14,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//Added Code for Assignment//
+//--------------------------------------------------------------------------------------------------------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+//Variables for each prompt
+//--------------------------------------------------------------------------------------------------------------------------------------------------//
+//Special Characters
+var spcCharacters = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `(`, `)`, `-`, `_`, `=`, `+`, `?`, `<`, `>`, `[`, `]`, `.`];
+//Numbered Characters
+var nmbCharacters = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
+//Lowercase Characters
+var lwrCaseChar   = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
+//UpperCase Characters
+var uppCaseChar   = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `R`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
+
+//Variables for the password prompts
+// the [] indicates empty variables that will be filled in 
+var passLength = [];
+console.log(passLength);
+var passOptions = [];
+//--------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+
+
+
