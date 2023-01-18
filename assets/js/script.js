4fc526f43a5481d1acf138c3528e7b08054ccc57
nshaw973 <nshaw973@gmail.com>
2023-01-17 17:45:04 -0800

    added code to the prompts that will check if the user had clicked no on every option. If they had clicked no an alert will pop up saying to please select 1 option

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,9 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-
 //Added Code for Assignment//
 //--------------------------------------------------------------------------------------------------------------------------------------------------//
 //--------------------------------------------------------------------------------------------------------------------------------------------------//
-
 
 //Variables for each prompt
 //--------------------------------------------------------------------------------------------------------------------------------------------------//
@@ -39,8 +36,78 @@ var uppCaseChar   = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`,
 var passLength = [];
 console.log(passLength);
 var passOptions = [];
+//optionWarning is to make sure the user chose at least one option before telling the user that there was an error and that there must be at least 1 option chosen
+var optionWarning = [];
 //--------------------------------------------------------------------------------------------------------------------------------------------------//
 
+//Function for the Prompts
+//--------------------------------------------------------------------------------------------------------------------------------------------------//
+function getPrompts() {
+  passLength = (window.prompt("Choose a password length between 8-128 characters:"))
+  console.log(passLength)
+  //isNaN is making sure what is entered is a number and then its making sure that number isn't less than 8 or greater than 128
+  if (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    alert("please choose between 8-128")
+    //closes the window when the wrong number is chosen after alerting the user that the number must be between 1-128
+    //if it failed to meet the criteria, it reverts passLength to 0 and creates the options to 0 to avoid creating an "Undefined" value
+    passLength = 0
+    passOptions = [];
+    return;
+  }
+
+
+  //adding confirm windows creates a true or false scenario where its either a yes or no situation.
+  if (confirm("Add lowercase letters?")){
+    //added console.log to see how each option is combined as user goes through each prompt    
+    //this passoptions is to clear out any previous options chosen on a previous generation to prevent the array from stacking on each other on a new generation
+    passOptions = [];
+    //the .push function is combine the options chosen into the passoptions
+    //optionWarning occurs everytime the user clicks cancel on one of the options, creating a tick where if all 4 options were denied, it creates a warning only when each else function has added up to 4.
+    passOptions.push(...lwrCaseChar);
+    console.log(passOptions);
+  } else {
+    optionWarning++;
+    console.log(optionWarning)
+  }
+
+  if (confirm("Add uppercase letters?")){
+    passOptions.push(...uppCaseChar);
+    console.log(passOptions);
+  } else {
+    optionWarning++;
+    console.log(optionWarning)
+  }
+
+  if (confirm("Add Special Characters?")){
+    passOptions.push(...spcCharacters);
+    console.log(passOptions);
+  } else {
+    optionWarning++;
+    console.log(optionWarning)
+  }
+
+  if (confirm("Add numbers?")){
+    passOptions.push(...nmbCharacters);
+    console.log(passOptions);
+  } else {
+    optionWarning++;
+    console.log(optionWarning)
+  }
+
+  if (optionWarning === 4){
+    alert("Please select at least one option!");
+    passLength = [];
+    passOptions = [];
+    return;
+  }
+
+  optionWarning = 0
+  console.log(optionWarning)
+
+  return;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------//
 
 
 //This function will generate the password based on what is given in the prompts
@@ -60,49 +127,3 @@ function generatePassword() {
   return genPass;
 }
 //--------------------------------------------------------------------------------------------------------------------------------------------------//
-
-
-
-//Function for the Prompts
-//--------------------------------------------------------------------------------------------------------------------------------------------------//
-function getPrompts() {
-  passLength = (window.prompt("Choose a password length between 8-128 characters:"))
-  console.log(passLength)
-  //isNaN is making sure what is entered is a number and then its making sure that number isn't less than 8 or greater than 128
-  if (isNaN(passLength) || passLength < 8 || passLength > 128) {
-    alert("please choose between 8-128")
-    //closes the window when the wrong number is chosen after alerting the user that the number must be between 1-128
-    //if it failed to meet the criteria, it reverts passLength to 0 and creates the options to 0 to avoid creating an "Undefined" value
-    passLength = 0
-    passOptions = [];
-    return;
-  }
-  //adding confirm windows creates a true or false scenario where its either a yes or no situation.
-  if (confirm("Add lowercase letters?")){
-    //concat is concatinate and its combining the choices into the passOptions array
-    //added console.log to see how each option is combined as user goes through each prompt
-    passOptions = passOptions.concat(lwrCaseChar);
-    console.log(passOptions)
-  }
-
-  if (confirm("Add uppercase letters?")){
-    passOptions = passOptions.concat(uppCaseChar);
-    console.log(passOptions)
-  }
-
-  if (confirm("Add Special Characters?")){
-    passOptions = passOptions.concat(spcCharacters);
-    console.log(passOptions)
-  }
-
-  if (confirm("Add numbers?")){
-    passOptions = passOptions.concat(nmbCharacters);
-    console.log(passOptions)
-  }
-  return;
-}
-
-//--------------------------------------------------------------------------------------------------------------------------------------------------//
-
-
-
